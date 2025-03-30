f7f853c091dcf01b43a694445b3ba2ae397bfa0d
maka70vv <25.makarovv@gmail.com>
2025-03-30 15:51:27 +0600

    added epub parser

--- parsers.py
+++ parsers.py
@@ -1,12 +1,26 @@
-import fitz
-
-
 def extract_text_from_pdf(pdf_path):
+    import fitz
+
     doc = fitz.open(pdf_path)  # Открываем PDF
     text = ""
 
     for page_num in range(doc.page_count):
         page = doc.load_page(page_num)  # Загружаем страницу
         text += page.get_text("text")  # Извлекаем текст из страницы
+
+    return text
+
+
+def extract_text_from_epub(epub_path):
+    import ebooklib
+    from ebooklib import epub
+    from bs4 import BeautifulSoup
+    book = epub.read_epub(epub_path)
+    text = ""
+
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            soup = BeautifulSoup(item.content, "html.parser")
+            text += soup.get_text()
 
     return text
